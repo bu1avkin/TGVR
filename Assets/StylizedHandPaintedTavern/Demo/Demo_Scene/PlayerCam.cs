0009cefcbf0450b32aab787684449c8f3554f26a
prodhidden <90219801+prodhidden@users.noreply.github.com>
2023-02-09 19:53:45 +0300

    alpha0.1
    [+] full-controlled player for Win/Mac OS

---
+++ Assets/StylizedHandPaintedTavern/Demo/Demo_Scene/PlayerCam.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float SensX;
+    public float SensY;
+    public Transform orientation;
+    float xRotation;
+    float yRotation;
+
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * SensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * SensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }
+}
